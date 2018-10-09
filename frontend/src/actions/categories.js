b21773501eff75ef0f38b095b94ed484a5d6c8e9
rila <fukushima06rila@gmail.com>
2018-10-09 15:36:54 +0800

    add action

---
+++ frontend/src/actions/categories.js
@@ -0,0 +1,29 @@
+import * as type from './ActionType'
+import { getCategories } from './../api';
+
+const api = "http://localhost:3001";
+
+const headers = {
+    'Authorization' : 'whatever-you-want'
+};
+
+export function getCategories(){
+    return (dispatch) =>{
+        return fetch(`${api}/categories`,{
+            headers
+        }).then((res) => {
+            return res.json();
+        }).then((data) => {
+            let categories = data.categories;
+            categories.unshift({name:'all',path:'all'});
+            dispatch(getCategoriesSuccess(categories))
+        });
+    }
+}
+
+export function getCategoriesSuccess(categories){
+    return {
+        type: type.GET_CATEGORIES,
+        categories
+    }
+}
