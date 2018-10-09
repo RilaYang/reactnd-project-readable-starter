e536a43808aa5115dd66eadcef07a4651c2c5dac
rila <fukushima06rila@gmail.com>
2018-10-09 17:56:34 +0800

    component

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import { Menu } from 'semantic-ui-react';
+
+class Nav extends Component {
+    render() {
+        const { categories, activateCategory, onClickHandler} = this.props;
+
+        return(
+            <Menu vertical fluid>
+             {
+                 categories.map((item) => {
+                     return <Menu.Item key={shortid.generate()}
+                                       name={item.name}
+                                       active={activateCategory === item.name}
+                                       onClick={onClickHandler}>{item.name}
+                                       </Menu.Item>
+                 })
+             }
+            </Menu>
+        )
+    }
+}
+
+export default Nav;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+import PostList from './PostList';
+import Nav from './Nav';
+
+export { PostList, Nav};
