a342b8124d5eb48d25c977137a3d9260ea33cf59
rila <fukushima06rila@gmail.com>
2018-10-10 10:47:49 +0800

    modify post.js

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -1,4 +1,5 @@
 import * as type from './ActionType';
+import PostList from './../components/PostList';
 
 const api = "http://localhost:3001";
 
@@ -22,5 +23,65 @@ function getPostsSuccess(posts){
     return{
         type: type.GET_POST,
         posts
+    }
+}
+
+export function getPostsDetail(id) {
+    return (dispatch) => {
+        return fetch(`${api}/posts/${id}`, {
+            headers
+        }).then((res) => {
+            return res.json();
+        }).then((data) => {
+            dispatch(getPostsDetailSuccess(data))
+        });
+    }
+}
+
+function getPostsDetailSuccess(post){
+    return{
+        type: type.GET_TARGET_POST,
+        post
+    }
+}
+
+export function getPostsComment(id) {
+    return (dispatch) => {
+        return fetch(`${api}/posts/${id}/comments`, {
+            headers
+        }).then((res) => {
+            return res.json();
+        }).then ((data) => {
+            dispatch(getPostsCommentSuccess(data))
+        });
+    }
+}
+
+function getPostsCommentSuccess(comments) {
+    return {
+        type: type.GET_POST_COMMENT,
+        comments
+    }
+}
+
+export const newPosts = (data) => {
+    return (dispatch) => {
+        return fetch(`${api}/posts`, {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json)
+        .then(data => dispatch(newPostsSuccess(data)))
+    }
+}
+
+function newPostsSuccess(post) {
+    return {
+        type: type.POST_NEW_POST,
+        post
     }
 }
